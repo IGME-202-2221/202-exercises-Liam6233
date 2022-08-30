58e5c70d984de531ac3d15af49c6d22bee91bc96
Liam Curley <lmc2239@rit.edu>
2022-08-30 10:37:52 -0400

    made it so camera can rotate in every direction

--- 8_30_2022 Demo/Assets/Script/Demo_Script.cs
+++ 8_30_2022 Demo/Assets/Script/Demo_Script.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class Demo_Script : MonoBehaviour
 {
     [SerializeField]
-    public float rotator;
+    public float rotatorX, rotatorY,rotatorZ;
     
     public Camera cam;
     // Start is called before the first frame update
     void Start()
     {
-        rotator = 0;
-        cam = new Camera();
+        
+        //cam = new Camera();
     }
     
     // gets called first frame that the script is enabled
@@ -24,15 +24,7 @@ public class Demo_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(rotator < 361)
-        {
-            rotator += 1f;
-        }
-        else
-        {
-            rotator = 0;
-        }
-        cam.transform.Rotate(new Vector3(0, rotator, 0));
-        
+
+        cam.transform.Rotate(new Vector3(rotatorX, rotatorY, rotatorZ));
     }
 }
