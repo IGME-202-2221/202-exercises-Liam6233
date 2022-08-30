3578c521cc60777f890126ec99707b06c77a6510
Liam Curley <lmc2239@rit.edu>
2022-08-30 10:26:37 -0400

    got a camera to rotate very fast

--- 8_30_2022 Demo/Assets/Script/Demo_Script.cs
+++ 8_30_2022 Demo/Assets/Script/Demo_Script.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Demo_Script : MonoBehaviour
 {
+    [SerializeField]
+    public float rotator;
+    
+    public Camera cam;
     // Start is called before the first frame update
     void Start()
+    {
+        rotator = 0;
+        cam = new Camera();
+    }
+    
+    // gets called first frame that the script is enabled
+    private void Awake()
     {
         
     }
@@ -13,6 +24,15 @@ public class Demo_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(rotator < 361)
+        {
+            rotator += 1f;
+        }
+        else
+        {
+            rotator = 0;
+        }
+        cam.transform.Rotate(new Vector3(0, rotator, 0));
         
     }
 }
