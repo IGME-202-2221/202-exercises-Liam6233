7b8bc99f0ec8ed3f9226559e8e82dc4aadd4a1c0
Liam Curley <lmc2239@g.rit.edu>
2022-09-10 10:53:13 -0400

    created clocknumbers script and got numbers positioned and displaying proper text using trig

--- Exercise 3/Assets/Scripts/ClockNumbers.cs
+++ Exercise 3/Assets/Scripts/ClockNumbers.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class ClockNumbers : MonoBehaviour
 {
+    [SerializeField]
+    GameObject clockNumberPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        GameObject clockNumber;
 
+        for (int i = 1; i <= 12; i++)
+        {
+            clockNumber = Instantiate(clockNumberPrefab);
+
+
+            //	Set Position
+            float angle = (Mathf.PI / 6) * i+1.5f;
+            clockNumber.transform.position = new Vector3(-Mathf.Cos(angle) * 2.25f, Mathf.Sin(angle) * 2.25f,0);
+            //	Set Text
+            clockNumber.GetComponent<TextMesh>().text = i.ToString();
+            
+        }
+    }
     // Update is called once per frame
     void Update()
     {
