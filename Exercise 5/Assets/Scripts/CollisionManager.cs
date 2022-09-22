4fefce87e85d96f27a85eecae7514198597b1c3c
liamc <lmc2239@g.rit.edu>
2022-09-22 16:07:56 -0400

    Got Circle and AABB collision working, Only after moving Collision methods into collision manager script. I no longer needed Collision detection script. Only problem now is that only 1 collideObject makes player turn when when colliding

--- Exercise 5/Assets/Scripts/CollisionManager.cs
+++ Exercise 5/Assets/Scripts/CollisionManager.cs
@@ -13,47 +13,47 @@ public class CollisionManager : MonoBehaviour
     [SerializeField]
     List<GameObject> obsticalList;
 
-    CollisionDetection dection;
+    [SerializeField]
+    GameObject colType; 
+
     // false = AABB true = Circle
-    bool collisionMethod = true;
+    bool collisionMethod = false;
     // Start is called before the first frame update
     void Start()
     {
         shipColor = ship.GetComponent<SpriteRenderer>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (collisionMethod)
+        GameObject player = ship;
+        GameObject rock;
+        bool collision;
+        for(int i = 0; i < obsticalList.Count; i++)
         {
-           for(int i = 0; i < obsticalList.Count; i++)
+            rock = obsticalList[i];
+            if (collisionMethod)
             {
-                if(dection.CircleCollision(ship, obsticalList[i])){
-                   shipColor.color = Color.red;
-                   obsticalList[i].GetComponent<SpriteRenderer>().color = Color.red;
-                }
-                else
-                {
-                    shipColor.color = Color.white;
-                    obsticalList[i].GetComponent<SpriteRenderer>().color = Color.red;
-                }
+                colType.GetComponent<TextMesh>().text = "Collision Mode: Circle Collision";
+                collision = CircleCollision(player, rock);
             }
-        }
-        else
-        {
-            for (int i = 0; i < obsticalList.Count; i++)
+            else
             {
-                if (dection.CircleCollision(ship, obsticalList[i]))
-                {
-                    shipColor.color = Color.red;
-                    obsticalList[i].GetComponent<SpriteRenderer>().color = Color.red;
-                }
-                else
-                {
-                    shipColor.color = Color.white;
-                    obsticalList[i].GetComponent<SpriteRenderer>().color = Color.red;
-                }
+                colType.GetComponent<TextMesh>().text = "Collision Mode: AABB Collision";
+                collision = AABBCollision(player, rock);
+            }
+
+            if (collision)
+            {
+                shipColor.color = Color.red;
+                obsticalList[i].GetComponent<SpriteRenderer>().color = Color.red;
+            }
+            else 
+            {
+                shipColor.color = Color.white;
+                obsticalList[i].GetComponent<SpriteRenderer>().color = Color.white;
             }
         }
     }
@@ -61,5 +61,36 @@ public class CollisionManager : MonoBehaviour
     public void ChangeCollision()
     {
         collisionMethod = !collisionMethod;
+    }
+    public bool AABBCollision(GameObject player, GameObject obstical)
+    {
+        bool areColliding = false;
+        Bounds playerBox = player.GetComponent<SpriteRenderer>().bounds;
+        Bounds obsticalBox = obstical.GetComponent<SpriteRenderer>().bounds;
+
+        if (obsticalBox.min.x < playerBox.max.x &&
+            obsticalBox.max.x > playerBox.min.x &&
+            obsticalBox.max.y > playerBox.min.y &&
+            obsticalBox.min.y < playerBox.max.y)
+        {
+            areColliding = true;
+        }
+        return areColliding;
+    }
+
+    public bool CircleCollision(GameObject player, GameObject obstical)
+    {
+        bool isColliding = false;
+        float playerRad = player.GetComponent<SpriteRenderer>().bounds.extents.x;
+        float obRad = obstical.GetComponent<SpriteRenderer>().bounds.extents.x;
+
+        float distance = Mathf.Pow(player.transform.position.x - obstical.transform.position.x, 2) 
+            + Mathf.Pow(player.transform.position.y - obstical.transform.position.y, 2);
+        if ((playerRad + obRad) > Mathf.Sqrt(distance))
+        {
+            isColliding = true;
+
+        }
+        return isColliding;
     }
 }
--- Exercise 5/Assets/Scripts/spriteInfo.cs
+++ Exercise 5/Assets/Scripts/spriteInfo.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class spriteInfo : MonoBehaviour
 {
-    
+    [SerializeField]
+    GameObject prefabObject;
+
+    Bounds spriteBox;
+    BoundingSphere colCircle;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteBox = prefabObject.GetComponent<SpriteRenderer>().bounds;
+        colCircle = new BoundingSphere(prefabObject.transform.position, spriteBox.max.x / 2);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+    }
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(prefabObject.transform.position, spriteBox.extents.x);
     }
 }
