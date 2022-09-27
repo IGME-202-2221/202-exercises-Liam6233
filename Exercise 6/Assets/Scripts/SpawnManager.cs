74c4728c25ec35d5e48ebb9c34f7eb6498896b73
Liam Curley <lmc2239@rit.edu>
2022-09-27 09:57:33 -0400

    created spawnManager object, worked on SpawnManager Script, created prefabs of all creatures

--- Exercise 6/Assets/Scripts/SpawnManager.cs
+++ Exercise 6/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+
+    private List<SpriteRenderer> animals = new List<SpriteRenderer>();
+
+    public SpriteRenderer elPrefab;
+    public SpriteRenderer turtPrefab;
+    public SpriteRenderer snailPrefab;
+    public SpriteRenderer octoPrefab;
+    public SpriteRenderer kangoPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Spawn();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Spawn()
+    {
+
     }
 }
